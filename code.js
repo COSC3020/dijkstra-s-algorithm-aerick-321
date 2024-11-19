64135691ac18d68708a6ee555a08a864c014648e
aerick-321 <158105735+aerick-321@users.noreply.github.com>
2024-11-19 16:44:39 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,19 @@
 function dijkstra(graph, sourceNode) {
     let dist = Infinity;
-    sourceNode = 0;
-    
-    return [];
+    let v = [];
+    let queue = new PriorityQueue();
+    queue.enqueue(sourceNode, 0);
+    while (queue != empty){
+        let u = queue.dequeue();
+        if (u === v) return v;
+        v.push(u);
+        for (let next = 0; graph.length(current); next++){
+             newdist = dist [u] + graph.length(u, next);
+            if (newdist < dist[next]){
+                dist[next] = newdist;
+                queue.enqueue(next, dist[next]);
+            }
+        }
+    }
+    return dist;
 }
