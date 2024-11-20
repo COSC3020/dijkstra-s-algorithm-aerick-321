cd09444357862e196aad99bb71e5f8b3b6852d31
aerick-321 <158105735+aerick-321@users.noreply.github.com>
2024-11-20 15:35:23 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,18 +1,25 @@
 function dijkstra(graph, sourceNode) {
-    let dist = [Infinity]; 
+    let dist = {};
+     for (let node in graph) {
+        dist[node] = Infinity; // Set all distances to infinity
+    }
+    
     dist [sourceNode] = 0;
-    let visited;
+    let visited = new Set();
     let queue = new PriorityQueue();
     queue.enqueue(sourceNode, 0);
+    
     while (!queue.isEmpty()){
         let u = queue.dequeue();
-        if (u in visited) {
+        if (visited.has(u)) {
            continue;
         }
+        
         visited.add(u);
         for (let next of graph[u]){
             let weight = graph[u][next]; 
             let newdist = dist[u] + weight;
+            
             if (newdist < dist[next]){
                 dist[next] = newdist;
                 queue.enqueue(next, dist[next]);
